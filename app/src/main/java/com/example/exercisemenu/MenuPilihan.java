8b909fecc08cd458d5bd2463874b79ea5cb6daf2
Your Name <andangsaefullah4@gmail.com>
2020-04-06 22:12:09 +0700

    add menu bangun datar

--- app/src/main/java/com/example/exercisemenu/MenuPilihan.java
+++ app/src/main/java/com/example/exercisemenu/MenuPilihan.java
@@ -3,6 +3,7 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MenuPilihan extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MenuPilihan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_pilihan);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        ImageView.(R.drawable)
+
     }
 }
