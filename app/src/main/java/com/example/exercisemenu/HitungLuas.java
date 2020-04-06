a91b1d683e61a880c3395231ddf9e2c5b1b860b1
Your Name <andangsaefullah4@gmail.com>
2020-04-06 18:55:51 +0700

    Layout Persegi dan hitung luas persegi

--- app/src/main/java/com/example/exercisemenu/HitungLuas.java
+++ app/src/main/java/com/example/exercisemenu/HitungLuas.java
@@ -3,12 +3,35 @@ package com.example.exercisemenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class HitungLuas extends AppCompatActivity {
+public class HitungLuas extends AppCompatActivity {\
+    TextView txtHasil;
+    EditText txtPanjang, txtLebar;
+    Button btnHitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hitung_luas);
+
+        txtHasil = (TextView)findViewById(R.id.txtHasil);
+        txtPanjang = (EditText) findViewById(R.id.txtPanjang);
+        txtLebar = (EditText) findViewById(R.id.txtLebar);
+        btnHitung = (Button) findViewById(R.id.btnHitungLuas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int p,l,h;
+                p = Integer.parseInt(txtPanjang.getText().toString());
+                l = Integer.parseInt(txtLebar.getText().toString());
+                h = p*l;
+                txtHasil.setText("Luas : " + h);
+            }
+        });
     }
 }
